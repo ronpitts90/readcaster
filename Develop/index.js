f68c03e8372a7cbb75448c7bd7053baa3cf97847
Ronald Pitts <ronpitts81@gmail.com>
2020-08-07 21:03:48 -0400

    adding some question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,29 @@
+var inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
 ];
+
+    inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ])
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {

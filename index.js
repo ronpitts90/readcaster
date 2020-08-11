ae038aea8f1ed673257f6c31c5f9e8dca0a33a9f
Ronald Pitts <ronpitts81@gmail.com>
2020-08-11 12:59:59 -0400

    new index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var generateMarkdown = require("./utils/generateMarkdown");
+var fs = require("fs");
 
 // 1. import generateMarkdown, and import fs
 
@@ -7,18 +8,38 @@ var generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    message: "What is your user name?",
+    message: "What is the title of this project?",
     name: "title",
   },
   {
-    type: "password",
-    message: "What is your password?",
-    name: "password",
+    type: "input",
+    message: "Describe your project.",
+    name: "description",
   },
   {
-    type: "password",
-    message: "Re-enter password to confirm:",
-    name: "confirm",
+    type: "input",
+    message: "How do you install the project?",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "What is the usage?",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "What are the contibution guidelines?",
+    name: "Contributing",
+  },
+  {
+    type: "input",
+    message: "What are the testing instructions?",
+    name: "Tests",
+  },
+  {
+    type: "input",
+    message: "What kind of license would you like your project to have?",
+    name: "License",
   },
 ];
 
@@ -32,11 +53,15 @@ function init() {
   inquirer.prompt(questions).then((results) => {
     console.log(results);
 
-    //2. call the generateMarkdown function and past in results and put it in a variable
+    //2. call the generateMarkdown function and pass in results and put it in a variable
 
-    var readmeTemplate = generateMarkdown(results);
+    var readTemplate = generateMarkdown(results);
 
     //3. call the writeTofile function and pass in the fileName -> "README.MD", and for data pass in the variable that you created that holds the generateMarkdown function from step2
+    function writeToFile(README , readTemplate) {
+     
+    }
+
   });
 }
 
